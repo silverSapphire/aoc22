2d9b612d71226033847e51c26968416654e7b2da
ky <ky@kyPC>
2022-11-30 22:51:43 -0800

    Day 2 solved

--- day1/main.py
+++ day1/main.py
@@ -11,9 +11,13 @@ def parseData(lines):
     nums = [int(x.strip()) if x.strip().isdigit() else None for x in lines]
     return [list(i) for match, i in it.groupby(nums, lambda p: p != None) if match]
 
-def solve(data):
-    return max([sum(x) for x in data])
+def solve1(data):
+    return max([sum(i) for i in data])
+
+def solve2(data):
+    return sum(sorted([sum(i) for i in data], reverse=True)[:3])
         
 if __name__ == "__main__":
     data = getData()
-    print(''.join(["The solution is ", str(solve(data))]))
+    print(''.join(["The solution to Part One is ", str(solve1(data))]))
+    print(''.join(["The solution to Part Two is ", str(solve2(data))]))
