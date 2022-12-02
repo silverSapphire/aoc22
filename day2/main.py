07801badde76d683ffd7f8e85dbcd62f1999afb5
ky <ky@kyPC>
2022-12-02 00:24:35 -0800

    Day 2 part 2 complete

--- day2/main.py
+++ day2/main.py
@@ -8,21 +8,18 @@ def getLines():
     return open('input.txt').readlines()
 
 def translate(x):
-    if x is "C" or x is "Z":
-        return 3
-    if x is "B" or x is "Y":
-        return 2
-    if x is "A" or x is "X":
-        return 1
+    m = {"C": 3, "Y": 3, "B": 2, "A": 1, "X": 0, "Z": 6}
+    return m[x]
 
-def outcomePts(i, j):
-    losses = [[1, 3], [2, 1], [3, 2]]
-    if i is j:
-        return 3
-    if [i, j] in losses:
-        return 0
+def symbolPts(i, j):
+    wins = {1: 2, 2: 3, 3: 1}
+    loses = {1: 3, 2: 1, 3: 2}
+    if j is 3:
+        return i
+    if j is 6:
+        return wins[i]
     else:
-        return 6
+        return loses[i]
 
 def parseData(lines):
     return map(translate, [i for j in [x.split() for x in lines] for i in j])
@@ -30,13 +27,9 @@ def parseData(lines):
 def solve1(plays):
     score = 0
     for i, j in zip(plays[0::2], plays[1::2]):
-        score = score + j + outcomePts(i, j)
+        score = score + j + symbolPts(i, j)
     return score
 
-#def solve2(data):
-#    return sum(sorted([sum(i) for i in data], reverse=True)[:3])
-        
 if __name__ == "__main__":
     data = getData()
     print(''.join(["The solution to Part One is ", str(solve1(data))]))
-#    print(''.join(["The solution to Part Two is ", str(solve2(data))]))
