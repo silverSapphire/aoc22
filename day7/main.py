18c5cf2c6cd00b51d87f3579ed3ba18983765019
ky <ky@kyPC>
2022-12-07 00:24:43 -0800

    Day 7 Part 1 solved

---
+++ day7/main.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+class Node:
+    def __init__(self, name):
+        self.name = name
+        self.size = 0
+        self.parent = None
+        self.children = []
+
+    def add_child(self, node):
+        node.parent = self
+        self.children.append(node)
+        self.add_size(node.size)
+
+    def add_size(self, size):
+        self.size = self.size + size
+        if self.parent: self.parent.add_size(size)
+
+    def find(self, x):
+        queue = [self]
+        visited = [self]
+
+        while queue:
+            node = queue.pop(0)
+            if node.name == x: return node
+            for child in node.children:
+                if child not in visited:
+                    visited.append(child)
+                    queue.append(child)
+
+    def find_size_thresh(self, thresh, list):
+        if self.size <= thresh: list.append(self)
+        for node in self.children:
+            node.find_size_thresh(thresh, list)
+
+def getData():
+    return [x for x in open('input.txt').readlines()]
+
+def parseData(lines):
+    lines = [x.replace("$", "").replace("ls", "").strip() for x in lines]
+    cmds = [x.split() for x in lines if x]
+
+    root = Node("/")
+    current = root
+    for c in cmds[1:]:
+        if c[0] == "cd":
+            if c[1] == "..": current = current.parent
+            else: current = current.find(c[1])
+        elif c[0] == "dir":
+            n = current.find(c[1])
+            if not n: 
+                n = Node(c[1])
+                current.add_child(n)
+        else:
+            current.add_size(int(c[0]))
+
+    return root
+
+def solve1(root):
+    littleNodes = []
+    root.find_size_thresh(100000, littleNodes)
+    return sum([x.size for x in littleNodes])
+
+if __name__ == "__main__":
+    root = parseData(getData())
+    print(''.join(["The solution to Part One is ", str(solve1(root))]))
