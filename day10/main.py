8ab43aa54be285ebee2ffbb3fdbe6b60b83d914c
silverSapphire <kyl.flu@gmail.com>
2023-01-04 20:41:44 -0700

    Day 10 part 2 complete

--- day10/main.py
+++ day10/main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+LEN = 40
+WIDTH = 6
+
 def getData():
     return [x.strip() for x in open('input.txt').readlines()]
 
@@ -34,6 +37,21 @@ def solve1(data):
 
     return sum
 
+def solve2(data):
+    states = process(data)
+    for i in range(WIDTH):
+        row = ""
+        for j in range(LEN):
+            s = states[i * LEN + j + 1]
+            if abs(j - s[1]) <= 1:
+                row = row + '#'
+            else:
+                row = row + '.'
+        print(row)
+
+    return None
+
 if __name__ == "__main__":
     data = parseData(getData())
     print(''.join(["The solution to Part One is ", str(solve1(data))]))
+    solve2(data)
